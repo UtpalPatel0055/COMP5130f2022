4cda88f8c5a2df93b475bb53321b074bf1d7b455
Utpal Patel <utpalpatel0055@gmail.com>
2022-11-08 21:20:39 -0500

    modified user.py file to add "/signup" end-point
    Created "/signup" functionality to add the user email into database

--- Weekely_Project_Progress_2/eAssessment/src/controllers/user.py
+++ Weekely_Project_Progress_2/eAssessment/src/controllers/user.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from src.models.user import User as UserModel
 
 user = Blueprint("user", __name__, url_prefix="/api/v1/user")
 
 @user.get('/')
 def Users():
     return 'Hello'
+
+@user.post("/signup")
+def SignUp():
+    email = request.json.get("email")
+    if UserModel.get_by_email(email=email) is not None:
+        return jsonify({"status": False, "message": "Email is already registered with us",  "tag": "danger"}), HTTP_409_CONFLICT
+    user = UserModel(email=email)
+    user.save()
+    #RegisterMaillable.send_mail(email, user.id)
+    return jsonify({"message": "New user created", "status": True}), 201
